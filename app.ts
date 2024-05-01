3014f7f4db8ee7a0333b9523790a91555fb1dff1
Raul <raulsanchezgranda@hotmail.com>
2024-05-02 00:46:41 +0200

    leccion 45

---
+++ app.ts
@@ -0,0 +1,13 @@
+
+
+let people: string[] = ['Fernando','Melisa','Eduardo'];
+let flowers:string []= ['Rosa','Girasol','Lirio','Rosa azul'];
+
+//if (people[3] ) {
+    
+ //   console.log(people[3]);
+//}
+
+for (let i= 0; i < flowers.length; i++) {
+    console.log(flowers[i]);
+}
